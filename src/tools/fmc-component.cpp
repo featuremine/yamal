b7d64843367efccde8e7520ef8235d39905b0b7f
Federico Ravchina <federico.ravchina@featuremine.com>
2022-07-21 10:27:16 -0300

    commit

---
+++ src/tools/fmc-component.cpp
@@ -0,0 +1,150 @@
+/******************************************************************************
+    COPYRIGHT (c) 2020 by Featuremine Corporation.
+    This software has been provided pursuant to a License Agreement
+    containing restrictions on its use.  This software contains
+    valuable trade secrets and proprietary information of
+    FeatureMine Corporation and is protected by law.  It may not be
+    copied or distributed in any form or medium, disclosed to third
+    parties, reverse engineered or used in any manner not provided
+    for in said License Agreement except with the prior written
+    authorization Featuremine Corporation.
+*****************************************************************************/
+
+#include <tclap/CmdLine.h>
+
+#include <fmc/component.h>
+#include <fmc/config.h>
+#include <fmc/signals.h>
+
+#include <fmc++/mpl.hpp>
+
+#include <ytp/version.h>
+
+#include <atomic>
+
+static std::atomic<bool> run = true;
+static void sig_handler(int s) { run = false; }
+
+struct deleter_t {
+  void operator()(struct fmc_component_module *ptr) { fmc_component_module_del(ptr); }
+  void operator()(struct fmc_cfg_sect_item *ptr) { fmc_cfg_sect_del(ptr); }
+};
+struct initdestroy_t {
+  void init(fmc_fd fd, const char *path) {
+    fmc_error_t *err;
+    fd = fmc_fopen(path, READ, &err);
+    fmc_runtime_error_unless(!err)
+          << "Unable to open file " << path << ": " << fmc_error_msg(err);
+  }
+  void destroy(fmc_fd fd) {
+    if (fd != -1) {
+      fmc_error_t *err;
+      fmc_fclose(fd, &err);
+      fmc_runtime_error_unless(!err)
+            << "Unable to close file " << fd << ": " << fmc_error_msg(err);
+    }
+  }
+
+  void init(struct fmc_component_sys &sys) {
+    fmc_component_sys_init(&sys);
+  }
+  void destroy(struct fmc_component_sys &sys) {
+    fmc_component_sys_destroy(&sys);
+  }
+};
+template<typename T, typename InitDestroy>
+struct owned_t {
+  template<typename ...Args>
+  owned_t(Args && ...args) {
+    InitDestroy().init(value, std::forward<Args>(args)...);
+  }
+  ~owned_t() {
+    InitDestroy().destroy(value);
+  }
+  owned_t(const owned_t &) = delete;
+  T value;
+};
+using module_ptr = std::unique_ptr<fmc_component_module, deleter_t>;
+using config_ptr = std::unique_ptr<fmc_cfg_sect_item, deleter_t>;
+using schema_ptr = struct fmc_cfg_node_spec *;
+using sys_ptr = struct fmc_cfg_node_spec *;
+
+int main(int argc, char **argv) {
+  fmc_set_signal_handler(sig_handler);
+
+  TCLAP::CmdLine cmd("ytp preempt allocation tool", ' ', YTP_VERSION);
+
+  TCLAP::ValueArg<std::string> mainArg(
+      "s", "section", "Main section to be used to load the module",
+      true, "main", "section");
+  cmd.add(mainArg);
+
+  TCLAP::ValueArg<std::string> cfgArg("c", "config", "Configuration path", true,
+                                      "config.ini", "config_path");
+  cmd.add(cfgArg);
+
+  TCLAP::UnlabeledValueArg<std::string> moduleArg("module", "Module name", true, "module", "module");
+  cmd.add(moduleArg);
+
+  TCLAP::UnlabeledValueArg<std::string> componentArg("component", "Component name", true, "component", "component");
+  cmd.add(componentArg);
+
+  cmd.parse(argc, argv);
+
+  // typedef type2 type_generic;
+
+  auto mod = load_module();
+  type_generic comptype = mod.get_type("oms");
+  auto spec = comptype.tp_cfgspec;
+
+
+  struct fmc_component_sys_wrapper {
+    fmc_component_sys_wrapper() {
+      fmc_component_sys_init(&sys);
+    }
+    ~fmc_component_sys_wrapper() {
+      fmc_component_sys_destroy(&sys);
+    }
+    struct fmc_component_sys sys;
+  } sys;
+
+  fmc_error_t *err;
+
+  auto module = module_ptr(fmc_component_module_new(&sys.sys, moduleArg.getValue().c_str(), &err));
+  fmc_runtime_error_unless(!err)
+        << "Unable to load module " << moduleArg.getValue() << ": " << fmc_error_msg(err);
+
+  config_ptr cfg;
+  {
+    schema_ptr schema = NULL;
+
+    struct file_wrapper {
+      explicit file_wrapper(const char *path) {
+      }
+      ~file_wrapper() {
+      }
+      fmc_fd fd;
+    } config_file(cfgArg.getValue().c_str());
+
+    cfg = config_ptr(fmc_cfg_sect_parse_ini_file(schema, config_file.fd, mainArg.getValue().c_str(), &err));
+  }
+
+  auto component = fmc_component_new(module.get(), componentArg.getValue().c_str(), cfg.get(), &err);
+  fmc_runtime_error_unless(!err)
+        << "Unable to load component " << componentArg.getValue() << ": " << fmc_error_msg(err);
+
+  if (component->_vt->tp_sched) {
+    while (run) {
+      // call sched on each component; return wallclock if any sched is null
+      fm_time64_t now = component->_vt->tp_sched(component);
+      component->_vt->tp_proc(component, now);
+    }
+  }
+  else {
+    while (run) {
+      component->_vt->tp_proc(component, fmc_cur_time_ns());
+    }
+  }
+
+  return 0;
+}
