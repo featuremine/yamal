30340aaa85cdfcf37568857f73bb444ca9159221
Maxim Trokhimtchouk <max@featuremine.com>
2022-08-05 17:34:59 -0400

    fix

--- include/fmc/math.h
+++ include/fmc/math.h
@@ -31,6 +31,16 @@
 extern "C" {
 #endif
 
+#define MAX(a,b) \
+  ({ __typeof__ (a) _a = (a); \
+     __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+#define MIN(a,b) \
+  ({ __typeof__ (a) _a = (a); \
+     __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
+
 #define fmc_sign(x) (((x) > 0) - ((x) < 0))
 
 /**
