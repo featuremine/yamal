ea2940594c0636c9bd4818f6005c6e5109131520
Andres Rangel <andres.rangel@featuremine.com>
2022-08-26 10:54:40 -0400

    fix

--- src/fmc/reactor.c
+++ src/fmc/reactor.c
@@ -91,7 +91,6 @@ static void utarr_init(void *elt) {
 struct fmc_reactor_ctx *fmc_reactor_ctx_new(struct fmc_reactor *reactor, fmc_error_t **error) {
   struct fmc_reactor_ctx *ctx = (struct fmc_reactor_ctx *)calloc(1, sizeof(*ctx));
   if (!ctx) goto cleanup;
-  memset(ctx, 0, sizeof(*ctx));
   ctx->reactor = reactor;
   ctx->idx = reactor->size;
   fmc_icd deps;
