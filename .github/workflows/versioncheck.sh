bdeedbe0852245fbf12c86c70233c349cb659e99
Leonardo Bonilla <leonardo.bonilla@featuremine.com>
2022-08-30 21:25:06 +0200

    Added newline, removed semicolon

--- .github/workflows/versioncheck.sh
+++ .github/workflows/versioncheck.sh
@@ -1,13 +1,13 @@
 python3 versioncheck.py;
 
 if [ $? -eq 0 ]; then 
-    echo Releasing new version v$(cat ../../VERSION);
+    echo "Releasing new version v$(cat ../../VERSION)"
     gh release create v$(cat ../../VERSION) --prerelease
 elif [ $? -eq 1 ]; then 
-    echo Correct version in version file before merging to main.;
+    echo "Correct version in version file before merging to main."
 elif [ $? -eq 2 ]; then 
-    echo Non-release event is not processed by this workflow.;
+    echo "Non-release event is not processed by this workflow."
 elif [ $? -eq 3 ]; then 
-    echo Releasing bug fix v$(cat ../../VERSION);
+    echo "Releasing bug fix v$(cat ../../VERSION)"
     gh release create v$(cat ../../VERSION) --prerelease
 fi 
