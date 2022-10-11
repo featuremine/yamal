1c0e8b3294a7b96628e72f759242f02f874aaa1c
Andres Rangel <andres.rangel@featuremine.com>
2022-10-11 16:56:00 -0400

    file header for source

--- src/ytp/api.cpp
+++ src/ytp/api.cpp
@@ -1,3 +1,26 @@
+/******************************************************************************
+
+        COPYRIGHT (c) 2022 by Featuremine Corporation.
+        This software has been provided pursuant to a License Agreement
+        containing restrictions on its use.  This software contains
+        valuable trade secrets and proprietary information of
+        Featuremine Corporation and is protected by law.  It may not be
+        copied or distributed in any form or medium, disclosed to third
+        parties, reverse engineered or used in any manner not provided
+        for in said License Agreement except with the prior written
+        authorization from Featuremine Corporation.
+
+ *****************************************************************************/
+
+/**
+ * @file api.cpp
+ * @date 4 Oct 2022
+ * @brief File contains implementation of yamal sequence API
+ *
+ * File contains implementation of yamal sequence API
+ * @see http://www.featuremine.com
+ */
+
 #include "ytp/api.h"
 #include "ytp/sequence.h"
 
