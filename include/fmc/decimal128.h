946be4e5dcf38bd1e32a173cf23537685595e6af
Andres Rangel <andres.rangel@featuremine.com>
2022-10-17 18:03:53 -0400

    defs and instructions to proceed

--- include/fmc/decimal128.h
+++ include/fmc/decimal128.h
@@ -14,9 +14,39 @@ typedef struct {
     uint8_t bytes[FMC_DECIMAL128_SIZE];
 } fmc_decimal128_t;
 
+// Pending:
+// To x and from x
+// MAX
+// MIN
+// Inplace operations
+
 FMMODFUNC void fmc_decimal128_from_str(fmc_decimal128_t *dest, const char* src);
+
 FMMODFUNC void fmc_decimal128_to_str(fmc_decimal128_t *src, char* dest);
+
+//extern decQuad * decQuadCompare(decQuad *, const decQuad *, const decQuad *, decContext *);
+//returns -1 for x < y, 0 for equal, 1 for higher
+//nan if x or y is nan
+FMMODFUNC bool fmc_decimal128_less(fmc_decimal128_t *lhs, fmc_decimal128_t *rhs);
+FMMODFUNC bool fmc_decimal128_greater(fmc_decimal128_t *lhs, fmc_decimal128_t *rhs);
+FMMODFUNC bool fmc_decimal128_equal(fmc_decimal128_t *lhs, fmc_decimal128_t *rhs);
+
+//extern decQuad * decQuadDivide(decQuad *, const decQuad *, const decQuad *, decContext *);
+FMMODFUNC void fmc_decimal128_divide(fmc_decimal128_t *res, fmc_decimal128_t *lhs, int64_t *rhs);
+
+//extern decQuad * decQuadDivideInteger(decQuad *, const decQuad *, const decQuad *, decContext *);
+FMMODFUNC void fmc_decimal64_intdiv(fmc_decimal128_t *res, fmc_decimal128_t *lhs, int64_t *rhs);
+
 FMMODFUNC void fmc_decimal128_add(fmc_decimal128_t *res, fmc_decimal128_t *lhs, fmc_decimal128_t *rhs);
+
+//extern decQuad * decQuadSubtract(decQuad *, const decQuad *, const decQuad *, decContext *);
+FMMODFUNC void fmc_decimal64_sub(fmc_decimal128_t *res, fmc_decimal128_t *lhs, fmc_decimal128_t *rhs);
+
+//extern decQuad * decQuadMultiply(decQuad *, const decQuad *, const decQuad *, decContext *);
+FMMODFUNC void fmc_decimal128_mul(fmc_decimal128_t *res, fmc_decimal128_t *lhs, fmc_decimal128_t *rhs);
+
+//extern decQuad * decQuadToIntegralValue(decQuad *, const decQuad *, decContext *, enum rounding);
+FMMODFUNC void fmc_decimal128_round(fmc_decimal128_t *res, fmc_decimal128_t *val);
 
 #ifdef __cplusplus
 }
