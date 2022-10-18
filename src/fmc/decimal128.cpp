34d4fd706b48f18b80f94b5a605c502afd085d6b
Andres Rangel <andres.rangel@featuremine.com>
2022-10-18 09:27:01 -0400

    api improvement to comply with decimal64 api

--- src/fmc/decimal128.cpp
+++ src/fmc/decimal128.cpp
@@ -18,10 +18,35 @@ void fmc_decimal128_from_str(fmc_decimal128_t *dest, const char* src) {
   decQuadFromString((decQuad *)dest, src, &wrap.set);
 }
 
-void fmc_decimal128_to_str(fmc_decimal128_t *src, char* dest) {
-  decQuadToString((decQuad *)src, dest);
+void fmc_decimal128_to_str(fmc_decimal128_t src, char* dest) {
+  decQuadToString((decQuad *)&src, dest);
 }
 
-void fmc_decimal128_add(fmc_decimal128_t *res, fmc_decimal128_t *lhs, fmc_decimal128_t *rhs) {
-  decQuadAdd((decQuad *)res, (decQuad *)lhs, (decQuad *)rhs, &wrap.set);
+//TODO: Confirm if it is possible to avoid int32 conversion and find out result from res
+bool fmc_decimal128_less(fmc_decimal128_t lhs, fmc_decimal128_t rhs) {
+  fmc_decimal128_t res;
+  decQuadCompare((decQuad*)&res, (decQuad*)&lhs, (decQuad*)&rhs, &wrap.set);
+  return decQuadToInt32((decQuad*)&res, &wrap.set, DEC_ROUND_HALF_UP) == -1;
+}
+bool fmc_decimal128_greater(fmc_decimal128_t lhs, fmc_decimal128_t rhs) {
+  fmc_decimal128_t res;
+  decQuadCompare((decQuad*)&res, (decQuad*)&lhs, (decQuad*)&rhs, &wrap.set);
+  return decQuadToInt32((decQuad*)&res, &wrap.set, DEC_ROUND_HALF_UP) == 1;
+}
+bool fmc_decimal128_equal(fmc_decimal128_t lhs, fmc_decimal128_t rhs) {
+  fmc_decimal128_t res;
+  decQuadCompare((decQuad*)&res, (decQuad*)&lhs, (decQuad*)&rhs, &wrap.set);
+  return decQuadToInt32((decQuad*)&res, &wrap.set, DEC_ROUND_HALF_UP) == 0;
+}
+
+fmc_decimal128_t fmc_decimal128_divide(fmc_decimal128_t lhs, int64_t *rhs) {
+  fmc_decimal128_t res;
+  decQuadDivide((decQuad *)&res, (decQuad *)&lhs, (decQuad *)&rhs, &wrap.set);
+  return res;
+}
+
+fmc_decimal128_t fmc_decimal128_add(fmc_decimal128_t lhs, fmc_decimal128_t rhs) {
+  fmc_decimal128_t res;
+  decQuadAdd((decQuad *)&res, (decQuad *)&lhs, (decQuad *)&rhs, &wrap.set);
+  return res;
 }
--- tests/fmc/decimal128.cpp
+++ tests/fmc/decimal128.cpp
@@ -8,8 +8,8 @@ TEST(decimal128, add) {
   char str[256];
   fmc_decimal128_from_str(&a, "11111111111.11111111111111111");
   fmc_decimal128_from_str(&b, "22222222222.22222222222222222");
-  fmc_decimal128_add(&a, &a, &b);
-  fmc_decimal128_to_str(&a, str);
+  a = fmc_decimal128_add(a, b);
+  fmc_decimal128_to_str(a, str);
   ASSERT_STREQ(str, "33333333333.33333333333333333");
 }
 
