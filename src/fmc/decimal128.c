2f8af6d6aa265ae41e64794b98200cb5b63bf4da
Andres Rangel <andres.rangel@featuremine.com>
2022-10-19 10:23:08 -0400

    file headers

--- src/fmc/decimal128.c
+++ src/fmc/decimal128.c
@@ -1,4 +1,24 @@
+/******************************************************************************
 
+        COPYRIGHT (c) 2022 by Featuremine Corporation.
+        This software has been provided pursuant to a License Agreement
+        containing restrictions on its use.  This software contains
+        valuable trade secrets and proprietary information of
+        Featuremine Corporation and is protected by law.  It may not be
+        copied or distributed in any form or medium, disclosed to third
+        parties, reverse engineered or used in any manner not provided
+        for in said License Agreement except with the prior written
+        authorization from Featuremine Corporation.
+
+*****************************************************************************/
+
+/**
+ * @file decimal.c
+ * @date 19 Oct 2022
+ * @brief Implementation of the fmc decimal API
+ *
+ * @see http://www.featuremine.com
+ */
 
 #include "decQuad.h"
 
--- tests/fmc/decimal128.cpp
+++ tests/fmc/decimal128.cpp
@@ -1,3 +1,24 @@
+/******************************************************************************
+
+        COPYRIGHT (c) 2022 by Featuremine Corporation.
+        This software has been provided pursuant to a License Agreement
+        containing restrictions on its use.  This software contains
+        valuable trade secrets and proprietary information of
+        Featuremine Corporation and is protected by law.  It may not be
+        copied or distributed in any form or medium, disclosed to third
+        parties, reverse engineered or used in any manner not provided
+        for in said License Agreement except with the prior written
+        authorization from Featuremine Corporation.
+
+ *****************************************************************************/
+
+/**
+ * @file decimal128.cpp
+ * @date 19 Oct 2022
+ * @brief File contains tests for decimal128
+ *
+ * @see http://www.featuremine.com
+ */
 
 #include "fmc/decimal128.h"
 #include "fmc++/decimal128.hpp"
