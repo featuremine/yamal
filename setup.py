fb190b8212d947b4d9a1f9e5dae195a0d3e576ce
Ivan <ivan.gonzalez@featuremine.com>
2022-11-08 14:38:15 -0300

    init into wheel

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setuptools.setup (
         'Programming Language :: Python :: 3 :: Only',
     ],
     package_data={
-        'yamal': ['*.so', '*.pyi','include', 'include/*', 'include/*/*', 'include/*/*/*']
+        'yamal': ['*.so', '*.py', '*.pyi','include', 'include/*', 'include/*/*', 'include/*/*/*']
     },
     license='COPYRIGHT (c) 2022 by Featuremine Corporation',
     packages=['yamal', 'yamal.tests'],
