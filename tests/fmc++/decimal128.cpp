b13c7d70aaecd8b054d1cd3a4b81305804ba60e4
Andres Rangel <andres.rangel@featuremine.com>
2022-11-10 22:36:56 -0500

    hash checks

--- tests/fmc++/decimal128.cpp
+++ tests/fmc++/decimal128.cpp
@@ -1793,77 +1793,92 @@ TEST(decimal128, cannonicalize) {
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 0);
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   fmc_error_t *err;
   fmc_decimal128_from_str(&a, "9999999999999999999999999999999999", &err);
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 0);
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   fmc_decimal128_from_str(&a, "-9999999999999999999999999999999999", &err);
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 0);
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   fmc_decimal128_from_str(&a, "9999999999999999999999999999999999E32", &err);
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 0);
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   fmc_decimal128_from_str(&a, "-9999999999999999999999999999999999E32", &err);
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 0);
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   a = 0;
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 34); // should it return zero?
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   a = 22;
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 0);
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   a = -22;
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 0);
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   fmc_decimal128_from_str(&a, "9099999999999999999999999999999999", &err);
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 0);
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   fmc_decimal128_from_str(&a, "-9099999999999999999999999999999999", &err);
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 0);
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   fmc_decimal128_from_str(&a, "9099999999999999999999999999999999E55", &err);
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 0);
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   fmc_decimal128_from_str(&a, "-9099999999999999999999999999999999E55", &err);
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 0);
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   a = 222;
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 0);
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   a = -222;
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 0);
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   fmc_decimal128_from_str(&a, "999999999999999999999999999999999", &err);
   fmc_decimal128_cannonicalize(&cannon, &a);
   ASSERT_EQ(fmc_decimal128_lead_zeros(&cannon), 0);
   ASSERT_EQ(a, cannon);
+  ASSERT_EQ(std::hash<fmc_decimal128_t>{}(a), std::hash<fmc_decimal128_t>{}(cannon));
 
   fmc_decimal128_from_str(&a, "999999999999999999999999999999999E55", &err);
   fmc_decimal128_cannonicalize(&cannon, &a);
