e5cab614f3d9659b4e452b7fb29d474651d959c6
Maxim Trokhimtchouk <max@featuremine.com>
2023-08-31 15:32:28 -0400

    update license and copyright

--- src/ytp/stream.c
+++ src/ytp/stream.c
@@ -1,16 +1,10 @@
 /******************************************************************************
-
-        COPYRIGHT (c) 2022 by Featuremine Corporation.
-        This software has been provided pursuant to a License Agreement
-        containing restrictions on its use.  This software contains
-        valuable trade secrets and proprietary information of
-        Featuremine Corporation and is protected by law.  It may not be
-        copied or distributed in any form or medium, disclosed to third
-        parties, reverse engineered or used in any manner not provided
-        for in said License Agreement except with the prior written
-        authorization from Featuremine Corporation.
-
-*****************************************************************************/
+        COPYRIGHT (c) 2019-2023 by Featuremine Corporation.
+        
+        This Source Code Form is subject to the terms of the Mozilla Public
+        License, v. 2.0. If a copy of the MPL was not distributed with this
+        file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *****************************************************************************/
 
 #include <fmc/error.h>
 
