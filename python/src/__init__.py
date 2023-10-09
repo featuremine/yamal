0a1e38f3b8291afd7080ab823d2caabac4c2707b
Andrés Rangel <andres.rangel@featuremine.com>
2023-10-09 10:48:48 -0400

    version fix

--- python/src/__init__.py
+++ python/src/__init__.py
@@ -16,6 +16,7 @@ if system.platform != "darwin":
 import json
 from operator import getitem
 from .yamal8 import *
+from .ytp import __version__
 
 class keydefaultdict(dict):
     def __init__(self, default_factory : Callable[[Any], Any]) -> None:
